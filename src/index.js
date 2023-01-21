acd90f62ec72d4ca34a347669f53fa28a3efe25d
EdwardTadeo <blacksstar98@gmail.com>
2023-01-20 23:30:20 -0500

    PC-20/01/23

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {CssBaseline } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Arial Black"', fontSize: 13
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: (theme) => ({
+        body: {
+          backgroundColor: theme.palette.primary.dark
+        }
+      })
+    }
+  }
+})
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
